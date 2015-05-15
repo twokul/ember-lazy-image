62c5a7907ca79b7cb5cbb82b6507f08113cf05b7
Alex Navasardyan <alex.navasardyan@gmail.com>
2015-05-15 11:11:06 -0700

    Revert img src to null
    fixes #29

--- addon/components/lazy-background-image.js
+++ addon/components/lazy-background-image.js
@@ -3,10 +3,8 @@ import ImageLoadMixin  from '../mixins/image-load';
 import LazyImageMixin  from '../mixins/lazy-image';
 import InViewportMixin from 'ember-in-viewport';
 
-var keys      = Ember.keys;
-var computed  = Ember.computed;
-var Component = Ember.Component;
-var forEach   = Ember.EnumerableUtils.forEach;
+const { keys, computed, Component, EnumerableUtils } = Ember;
+const forEach = EnumerableUtils.forEach;
 
 export default Component.extend(InViewportMixin, ImageLoadMixin, LazyImageMixin, {
   attributeBindings: ['width', 'height', 'style'],
@@ -18,12 +16,11 @@ export default Component.extend(InViewportMixin, ImageLoadMixin, LazyImageMixin,
   classNames: ['lazy-background-image'],
 
   _setupAttributes: function() {
-    var div       = this.$();
-    var component = this;
+    const component = this;
 
-    forEach(keys(component), function(key) {
+    forEach(keys(component), (key) => {
       if (key.substr(0, 5) === 'data-' && !key.match(/Binding$/)) {
-        div.attr(key, component.get(key));
+        component.get('attributeBindings').pushObject(key);
       }
     });
   }
--- addon/components/lazy-image.js
+++ addon/components/lazy-image.js
@@ -3,14 +3,8 @@ import ImageLoadMixin  from '../mixins/image-load';
 import LazyImageMixin  from '../mixins/lazy-image';
 import InViewportMixin from 'ember-in-viewport';
 
-var on        = Ember.on;
-var get       = Ember.get;
-var set       = Ember.set;
-var keys      = Ember.keys;
-var computed  = Ember.computed;
-var dasherize = Ember.String.dasherize;
-var Component = Ember.Component;
-var forEach   = Ember.EnumerableUtils.forEach;
+const { on, get, set, keys, computed, Component, EnumerableUtils } = Ember;
+const forEach = EnumerableUtils.forEach;
 
 export default Component.extend(InViewportMixin, ImageLoadMixin, LazyImageMixin, {
   classNames: ['lazy-image-container'],
@@ -20,15 +14,15 @@ export default Component.extend(InViewportMixin, ImageLoadMixin, LazyImageMixin,
   class: ['lazy-image'],
 
   _classJoin: on('init', function() {
-    var classArray = get(this, 'class');
+    const classArray = get(this, 'class');
     set(this, 'class', classArray.join(' '));
   }),
 
   _setupAttributes: function() {
-    var img       = this.$('img');
-    var component = this;
+    const img       = this.$('img');
+    const component = this;
 
-    forEach(keys(component), function(key) {
+    forEach(keys(component), (key) => {
       if (key.substr(0, 5) === 'data-' && !key.match(/Binding$/)) {
         img.attr(key, component.get(key));
       }
@@ -36,6 +30,6 @@ export default Component.extend(InViewportMixin, ImageLoadMixin, LazyImageMixin,
   },
 
   useDimensionsAttrs: computed('width', 'height', function() {
-    return ! this.get('width') || ! this.get('height') ? false : true;
+    return !this.get('width') || !this.get('height') ? false : true;
   })
 });
