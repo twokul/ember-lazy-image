85c2e611c1d1a3d40f5f69b175b19fffaca2d381
Karan Thakkar <karanjthakkar@gmail.com>
2015-06-26 16:06:54 +0530

    Attempt to resolve failing tests

--- addon/components/lazy-background-image.js
+++ addon/components/lazy-background-image.js
@@ -3,7 +3,7 @@ import ImageLoadMixin  from '../mixins/image-load';
 import LazyImageMixin  from '../mixins/lazy-image';
 import InViewportMixin from 'ember-in-viewport';
 
-const { keys, computed, Component, EnumerableUtils } = Ember;
+const { computed, Component, EnumerableUtils } = Ember;
 const forEach = EnumerableUtils.forEach;
 
 export default Component.extend(InViewportMixin, ImageLoadMixin, LazyImageMixin, {
@@ -21,7 +21,7 @@ export default Component.extend(InViewportMixin, ImageLoadMixin, LazyImageMixin,
   _setupAttributes: function() {
     const component = this;
 
-    forEach(keys(component), (key) => {
+    forEach(Object.keys(component), (key) => {
       if (key.substr(0, 5) === 'data-' && !key.match(/Binding$/)) {
         component.get('attributeBindings').pushObject(key);
       }
