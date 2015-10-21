482acaf438425fa2093800e6f3a0013ab16465a7
Kyle Turney <kyle@dialexa.com>
2015-10-21 16:40:19 -0500

    let's try to fix the build

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,6 +2,14 @@
 
 module.exports = {
   scenarios: [{
+    name: 'ember-1.13',
+    dependencies: {
+      'ember': 'components/ember#1.13'
+    },
+    resolutions: {
+      'ember': '1.13'
+    }
+  }, {
     name: 'ember-release',
     dependencies: {
       'ember': 'components/ember#release'
