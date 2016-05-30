52f4d4173028156d8a1b6d52a2095d506684e82f
Ivan <kobzar.ivan1@gmail.com>
2016-05-30 14:56:48 +0300

    Exclude css styles

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ module.exports = {
 
   included: function emberLazyImageIncluded(app) {
     this._super.included(app);
-
+    
+    if(app.options.imageLazyLoad && app.options.imageLazyLoad.excludeCss){ return; }
+    
     app.import('vendor/lazy-image/lazy-image.css');
   }
 };
