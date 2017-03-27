1da1914f06b1cf0e2d6e3e4e2b437eece7a1d718
Kevin Gomez <kkristoffergomez@gmail.com>
2017-03-27 11:18:28 -0500

    Oh wait I need this

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -11,6 +11,18 @@ export default Ember.Route.extend({
     }, {
       text: 'Godfrey Chan',
       url:  'https://avatars0.githubusercontent.com/u/55829?v=3&s=400'
+    }, {
+      text: 'Leah Silber',
+      url:  'https://avatars2.githubusercontent.com/u/25433?v=3&s=400'
+    }, {
+      text: 'Erik Bryn',
+      url:  'https://avatars0.githubusercontent.com/u/3018?v=3&s=400'
+    }, {
+      text: 'Kris Selden',
+      url:  'https://avatars0.githubusercontent.com/u/61024?v=3&s=400'
+    }, {
+      text: 'Stefan Penner',
+      url:  'https://avatars3.githubusercontent.com/u/1377?v=3&s=400'
     }]);
   }
 });
